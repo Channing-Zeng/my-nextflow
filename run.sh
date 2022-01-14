19f1d4a84f8166825bc47edffd4b5c234e11890a
Channing-Zeng <40817644+Channing-Zeng@users.noreply.github.com>
2022-01-15 05:08:43 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #nextflow run script7.nf --reads 's3://zenofbucket/nf-testing/*_{1,2}.fq'  --outdir 's3://zenofbucket/nf-testing/results/' -profile cloud -w 's3://zenofbucket/nf-testing/work/' -process.queue 'queue_test_light'
 
-nextflow run test.nf --reads 's3://zenofbucket/nf-testing/*_{1,2}.fq'  --outdir 's3://zenofbucket/nf-testing/results/' -profile cloud -w 's3://zenofbucket/
-nf-testing/work/' -process.queue 'queue_test_light'
+nextflow run test.nf --reads 's3://zenofbucket/nf-testing/*_{1,2}.fq'  --outdir 's3://zenofbucket/nf-testing/results/' -profile cloud -w 's3://zenofbucket/nf-testing/work/' -process.queue 'queue_test_light' --with-container
 
 
 
